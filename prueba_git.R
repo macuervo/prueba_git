fe48b51c48092e1d4fcf370600ae2ad4019f9c1f
macuervo <marat2313>
2024-04-20 11:49:53 -0500

    subalo hptagit status

---
+++ prueba_git.R
@@ -0,0 +1,10 @@
+install.packages('googlesheets4')
+library(googlesheets4)
+
+# The data is publicly available so we don't need to authenticate
+gs4_deauth()
+
+survey_data <- read_sheet("https://docs.google.com/spreadsheets/d/13kfPtyQP1xmL4Rn6rfJHgJcAblfH7pxS5RvdmGe6BHg/edit?usp=sharing")
+
+survey_data |>
+  saveRDS("survey_data.rds")
